73463ca4aa5dcce2be4893a44b4a22fdad7075df
Ramil Aliev <bezdonas@gmail.com>
2023-12-19 22:11:06 +0100

    Visual update and cleanup

--- prototypes/alpha-godot/scripts/Game.cs
+++ prototypes/alpha-godot/scripts/Game.cs
@@ -1,4 +1,3 @@
-using System;
 using Godot;
 
 public enum BuildMode
@@ -18,45 +17,16 @@ public partial class Game : Node
 
 	public void SetActiveBuildMode(BuildMode mode)
 	{
-		this.activeBuildMode = mode;
+		activeBuildMode = mode;
 		GD.Print("Active build mode changed to " + mode);
 		EmitSignal(SignalName.ActiveBuildModeChanged);
 	}
 
-	private void BuildHouseButtonEventHandler(InputEventMouseButton @event)
-	{
-		if (@event is InputEventMouseButton && @event.Pressed == true)
-		{
-			GD.Print("house click");
-			this.SetActiveBuildMode(BuildMode.Home);
-		}
-	}
-
-	private void BuildJobButtonEventHandler(InputEventMouseButton @event)
-	{
-		if (@event is InputEventMouseButton && @event.Pressed == true)
-		{
-			GD.Print("job click");
-			this.SetActiveBuildMode(BuildMode.Job);
-		}
-	}
-
-	private void BuildRoadButtonEventHandler(InputEventMouseButton @event)
-	{
-		if (@event is InputEventMouseButton && @event.Pressed == true)
-		{
-			GD.Print("road click");
-			this.SetActiveBuildMode(BuildMode.Road);
-		}
-	}
-
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print("Game ready");
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
